0b02a9699c1140287ef049a904010020c2d8eaaa
Alex <navia09alex64@gmail.com>
2021-04-23 19:48:25 -0500

    added more comments/ removed some testing code that i had/added a placeholder for the select button

--- app.js
+++ app.js
@@ -10,34 +10,35 @@ app.use(bodyParser.urlencoded());
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// Home page of the website
+// Home page of the website, will render the index page which is the home page
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.post('/selection', function(req,res){
-    var d = req.body.lans
-    console.log(d)
-});
 
-// form where the data from the selected language goes into 
+// the data from the selected language will be inserted into this endpoint using post
 app.post('/process_form', function(req, res){
+
     // created a variable to hold the selected input
     var lang = req.body.lang
 
     // print variable username to console
     console.log(lang);
+
     // using axios we get the data from the API that was provided
+    // will be using the GET method 
     axios.get(`https://cwrvx8v6xj.execute-api.us-east-2.amazonaws.com/default/apitest`)
     .then((response) => {
-        // the data from the api is put into the variable data
+        // the data from the api is put into the variable 'data'
         var data = response.data;
         // i created a for loop in order to check the every language in the api data
-        // to see if the matches the input selected from the user
+        // to see if it matches the input selected from the user
         // so it if does match then it loads the data for the language into the select page
         for (var i = 0; i<=4; i++){
             if (data[i].name == lang){
+                // console log to check if it is working and which one is selected
                 console.log(data[i])
+                // rendering the select page with the data from the api
                 res.render('pages/select.ejs',
                 {
                     //  from the api we take the name, date created, 
